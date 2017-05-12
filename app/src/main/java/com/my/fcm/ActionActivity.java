766c7aaf4519612a93669badd4a34564678ede2b
VisualGhost <Eugene.Ovsiy@dataart.com>
2017-05-12 12:53:58 +0300

    Add Activity to be opened when click on Notification

---
+++ app/src/main/java/com/my/fcm/ActionActivity.java
@@ -0,0 +1,18 @@
+package com.my.fcm;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by eovsiy on 5/12/2017.
+ */
+
+public class ActionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acvtivity_action);
+    }
+}
